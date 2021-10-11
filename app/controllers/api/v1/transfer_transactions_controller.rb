1abde2827dafa414d537b34ad4fb6500516962fa
Vlad Ermolovich <vlad.ermolovich@external.tablecheck.com>
2021-10-11 21:09:54 +0300

    Refactor broadcasting

--- app/controllers/api/v1/transfer_transactions_controller.rb
+++ app/controllers/api/v1/transfer_transactions_controller.rb
@@ -5,49 +5,19 @@ module Api
 
       skip_before_action :verify_authenticity_token
       before_action :initialize_accounts
-      # after_action :broadcast_updates, only: :create
 
       def create
         TransferService.new(@sender_account, @receiver_account, params[:amount]).transfer
-        broadcast_updates
         render json: [@sender_account, @receiver_account]
       end
 
       private
 
       def initialize_accounts
-        @accounts = params.values_at(:from, :to).map do |email|
+        @sender_account, @receiver_account = params.values_at(:from, :to).map do |email|
           user = UserService.new(email).find_or_create_user
           user.account
         end
-        @sender_account, @receiver_account = @accounts
-      end
-
-      def broadcast_updates
-        remove_outdated_accounts_from_dom
-        insert_updated_accounts_into_dom
-        cable_ready.broadcast
-      end
-
-      def selectors_for_remove
-        selectors_for_remove = @accounts
-          .map { |account| ".account[data-id=\"#{account.id}\"]" }
-          .join(', ')
-      end
-
-      def remove_outdated_accounts_from_dom
-        cable_ready['accounts'].remove(
-          selector: selectors_for_remove,
-          select_all: true
-        )
-      end
-
-      def insert_updated_accounts_into_dom
-        cable_ready['accounts'].insert_adjacent_html(
-          selector: '#accounts',
-          position: 'afterbegin',
-          html: render_to_string(collection: @accounts, partial: 'accounts/account')
-        )
       end
     end
   end
--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,6 @@
 class Account < ApplicationRecord
+  include CableReady::Broadcaster
+
   DEFAULT_BALANCE = 1000
 
   belongs_to :user, required: true
@@ -6,6 +8,7 @@ class Account < ApplicationRecord
   validates :balance, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   after_initialize :set_defaults, unless: :persisted?
+  after_commit :notify_observers, on: :update
 
   def sufficient_balance?(withdrawal_amount:)
     balance >= withdrawal_amount
@@ -16,4 +19,27 @@ class Account < ApplicationRecord
   def set_defaults
     self.balance = DEFAULT_BALANCE
   end
+
+  def notify_observers
+    remove_outdated_accounts_from_dom
+    insert_updated_accounts_into_dom
+    cable_ready.broadcast
+  end
+
+  def remove_outdated_accounts_from_dom
+    cable_ready['accounts'].remove(
+      selector: ".account[data-id=\"#{id}\"]",
+      select_all: true
+    )
+  end
+
+  def insert_updated_accounts_into_dom
+    cable_ready['accounts'].insert_adjacent_html(
+      selector: '#accounts',
+      position: 'afterbegin',
+      html: render(self)
+    )
+  end
+
+  delegate :render, to: :ApplicationController
 end
