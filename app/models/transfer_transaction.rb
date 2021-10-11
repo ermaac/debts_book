affe88360c5aad0f4b388d1c57f92faedaf820b9
Vlad Ermolovich <vlad.ermolovich@external.tablecheck.com>
2021-10-11 20:31:30 +0300

    Add self-transfer validation

--- app/models/transfer_transaction.rb
+++ app/models/transfer_transaction.rb
@@ -3,4 +3,11 @@ class TransferTransaction < ApplicationRecord
   belongs_to :receiver_account, class_name: 'Account', required: true
 
   validates_numericality_of :amount, greater_than: 0
+  validate :validate_transfer_accounts_differ!
+
+  def validate_transfer_accounts_differ!
+    return unless sender_account_id == receiver_account_id
+
+    errors.add(:base, 'Transfer accounts should be different')
+  end
 end
